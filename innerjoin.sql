fc2697642c505747ddf79d9b05e0f5020fbb7a97
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-04 15:27:36 -0700

    SQL statement to return contact for categories

--- innerjoin.sql
+++ innerjoin.sql
@@ -1,6 +1,11 @@
 
+/* Return issues table with category and subcategory */
 SELECT issues.room_id, issues.category_id, categories.category, 
 issues.subcategory_id, subcategories.subcategory ,issues.date 
 FROM issues 
 INNER JOIN categories ON issues.category_id= categories.id AND issues.room_id = 10 
 INNER JOIN subcategories ON issues.subcategory_id = subcategories.id;
+
+/*Return all contact with specific category*/
+SELECT  contacts.category_id, categories.category, contacts.name, FROM contacts
+INNER JOIN categories ON contacts.category_id = categories.id ORDER BY categories ASC;
