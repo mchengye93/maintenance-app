df30873e0ef561f22bd24e6814193e7bbf51bc5c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-04 15:24:04 -0700

    inner join tables to return issues

--- innerjoin.sql
+++ innerjoin.sql
@@ -1,1 +1,6 @@
-SELECT issues.room_id, issues.category_id, categories.category, issues.subcategory_id, subcategories.subcategory ,issues.date FROM issues INNER JOIN categories ON issues.category_id= categories.id AND issues.room_id = 10 INNER JOIN subcategories ON issues.subcategory_id = subcategories.id;
+
+SELECT issues.room_id, issues.category_id, categories.category, 
+issues.subcategory_id, subcategories.subcategory ,issues.date 
+FROM issues 
+INNER JOIN categories ON issues.category_id= categories.id AND issues.room_id = 10 
+INNER JOIN subcategories ON issues.subcategory_id = subcategories.id;
