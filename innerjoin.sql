64ad6bb0a570adf5feca60b2e262844ebe66622c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-04 15:36:18 -0700

    SQL to get all issues by specific categgory and ordered by data and room number

--- innerjoin.sql
+++ innerjoin.sql
@@ -8,4 +8,7 @@ INNER JOIN subcategories ON issues.subcategory_id = subcategories.id;
 
 /*Return all contact with specific category*/
 SELECT  contacts.category_id, categories.category, contacts.name, contacts.phone, contacts.email FROM contacts
-INNER JOIN categories ON contacts.category_id = categories.id ORDER BY categories.category,contacts.name ASC;
+INNER JOIN categories ON contacts.category_id = categories.id 
+ORDER BY categories.category,contacts.name ASC;
+
+/* Return all issues by specific category*/
