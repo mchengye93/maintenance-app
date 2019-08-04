f2376efbae5b977448f07988d3fc12a14b2cf7e0
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-04 13:23:56 -0700

    fix phone number fortmat script

--- contactGenerator.js
+++ contactGenerator.js
@@ -10,12 +10,12 @@ for (let i = 1; i <= 10; i += 1) {
 
   const name = faker.name.findName();
 
-  const phone = faker.phone.phoneNumber();
+  const phone = faker.phone.phoneNumberFormat(1);
 
   const email = faker.internet.email();
 
 
-  const contactRecord = `${i},${categoryId},${name}, ${phone},${email}\n`;
+  const contactRecord = `${i},${categoryId},${name},${phone},${email}\n`;
   // CREATE TABLE contact (
   //     id serial PRIMARY KEY,
   //     categoryId INT NOT NULL,
