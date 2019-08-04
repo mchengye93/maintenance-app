3bba54dd1745833f52e1f5d8536f60b32acb374a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-04 12:34:46 -0700

    added subcategory to script

--- issuesGenerator.js
+++ issuesGenerator.js
@@ -8,11 +8,32 @@ var writeStream = fs.createWriteStream('rooms.csv');
 writeStream.write('id,roomId,category,subcategory,date, photoUrl,description,cost,dateFixed\n');
 
 
-for (var i = 1; i <= 100; i++) {
-  var roomId = faker.random.number({min:1, max:5});
-  var parking = true;
+for (var i = 1; i <= 1000; i++) {
+  var roomId = faker.random.number({min:1, max:100});
+  var category = faker.random.number({min:1, max:5});
+  
+  var subcategory;
+  switch (category) {
+
+      case 1:
+        subcategory = faker.random.number({min:1, max:8});
+        break;
+      case 2:
+        subcategory = faker.random.number({min:9, max:13});
+        break;
+      case 3:
+        subcategory = faker.random.number({min:14, max:17});
+        break;
+      case 4:
+        subcategory = faker.random.number({min:18, max:21});
+        break;
+      case 5:
+        subcategory = faker.random.number({min:22, max:23});
+        break;
+  }
   
   
+
   var issueRecord = i + ',' + vip + ',' + parking + '\n';
   // write some data with a base64 encoding
   //   var stockRecord = {
