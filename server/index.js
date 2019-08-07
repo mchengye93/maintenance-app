837f29dd218cb09052111368bc7e365962c0e2bc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-06 23:34:27 -0700

    Setup server file

--- server/index.js
+++ server/index.js
@@ -5,3 +5,8 @@ const cors = require('cors');
 const issues = require('../database-postgres');
 
 const app = express();
+
+app.use(express.static(`${__dirname}/../react-client/dist`));
+
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/json
+app.use(bodyParser.json());
