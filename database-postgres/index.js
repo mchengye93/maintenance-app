dd5835e823328340dd6857a322e209b25d8f96d9
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-06 23:47:35 -0700

    Issues query added to database

--- database-postgres/index.js
+++ database-postgres/index.js
@@ -7,3 +7,22 @@ const postgresDb = new Pool({
   password: 'mapo',
   port: 5432,
 });
+
+const issues = function (callback) {
+  postgresDb.query(
+    'SELECT issues.room_id, issues.category_id, categories.category, issues.subcategory_id, subcategories.subcategory ,issues.date'
+        + 'FROM issues'
+        + 'INNER JOIN categories ON issues.category_id= categories.id AND issues.room_id = 10'
+        + 'INNER JOIN subcategories ON issues.subcategory_id = subcategories.id', (err, results) => {
+      if (err) {
+        callback(err, null);
+      }
+      callback(null, results.rows);
+    },
+  );
+};
+
+module.exports = {
+  issues,
+
+};
