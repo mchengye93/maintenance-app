317c42274c57a01419f6d3678348a1eeeb2afcea
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-08 14:41:23 -0700

    Return all unsolved issues

--- database-postgres/innerjoin.sql
+++ database-postgres/innerjoin.sql
@@ -14,7 +14,13 @@ SELECT * FROM issues ORDER BY date ASC;
 /*
 Return all unsolved issues
 */
-SELECT * FROM issues WHERE dateFixed IS NULL;
+SELECT issues.room_id, issues.category_id, categories.category, 
+issues.subcategory_id, subcategories.subcategory ,issues.date 
+FROM issues 
+INNER JOIN categories ON issues.category_id= categories.id 
+INNER JOIN subcategories ON issues.subcategory_id = subcategories.id
+WHERE dateFixed IS NULL
+ORDER BY date ASC;
 
 /*
 Return all unsolved issues that have more than a week without being solved
