55a50b91377c2167d2bf2fe7bdc3907dd17d82fb
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-08 23:41:42 -0700

    Return all issues by date

--- database-postgres/innerjoin.sql
+++ database-postgres/innerjoin.sql
@@ -13,7 +13,8 @@ SELECT issues.room_id, issues.category_id, categories.category,
 issues.subcategory_id, subcategories.subcategory ,issues.date 
 FROM issues 
 INNER JOIN categories ON issues.category_id= categories.id AND issues.room_id = 10 
-INNER JOIN subcategories ON issues.subcategory_id = subcategories.id;
+INNER JOIN subcategories ON issues.subcategory_id = subcategories.id
+ORDER BY date ASC;
 
 
 /*
