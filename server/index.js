aebb0033076f80db98db276fb434513d5a121930
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-08 23:46:39 -0700

    Setup server

--- server/index.js
+++ server/index.js
@@ -4,9 +4,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const issues = require('../database-postgres');
 
+const port = 3000;
 const app = express();
 
-app.use(express.static(`${__dirname}/../react-client/dist`));
+// app.use(express.static(`${__dirname}/../react-client/dist`));
 
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/json
 app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+;
