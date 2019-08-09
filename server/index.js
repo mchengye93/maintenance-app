7a78e3dc5c2cbc7ca190dfc5785ce2d0a46ff70c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-09 00:23:53 -0700

    API for categories/subcategories

--- server/index.js
+++ server/index.js
@@ -42,4 +42,14 @@ app.get('/api/subcategories', (req, res) => {
   });
 });
 
+app.get('/api/categoriessubcategories', (req, res) => {
+  issues.getAllCategoriesSubcategories((err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+    res.json(data);
+  });
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
