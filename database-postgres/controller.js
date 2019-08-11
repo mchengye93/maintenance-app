a3cbd01a432991eac6c117c22314fa8cd643ac98
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-10 23:00:43 -0700

    Fixed getIssue fucntion in controller

--- database-postgres/controller.js
+++ database-postgres/controller.js
@@ -18,11 +18,10 @@ const getAllIssues = (callback) => {
 const getIssue = (issueId, callback) => {
   console.log('inside db getIssue', issueId);
   connection.query(
-    `${'SELECT issues.room_id, issues.category_id, categories.category, issues.subcategory_id, subcategories.subcategory ,issues.date FROM issues '
+    `${'SELECT issues.id, issues.room_id, issues.category_id, categories.category, issues.subcategory_id, subcategories.subcategory ,issues.date FROM issues '
     + 'INNER JOIN categories ON issues.category_id= categories.id '
     + 'INNER JOIN subcategories ON issues.subcategory_id = subcategories.id '
-    + 'WHERE issues.id = '}${issueId
-    }${+'ORDER BY date ASC'}`,
+    + 'WHERE issues.id ='}${issueId}`,
     (err, results) => {
       if (err) {
         console.log(err);
