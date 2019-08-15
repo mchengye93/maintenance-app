b8ec42efc5584971c8e92a5c4fec75b199bdb534
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-14 23:00:24 -0700

    Create contact function on API call

--- server/index.js
+++ server/index.js
@@ -111,7 +111,12 @@ app.get('/api/categoriessubcategories', (req, res) => {
 });
 
 app.post('/api/contacts', (req, res) => {
-
+  issues.createContact(req.body, (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+    res.json(data);
+  });
 });
 
 app.get('/api/contacts', (req, res) => {
