3b9bedaaf9b1c56989cd9784639aec58574460a4
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-19 21:55:57 -0700

    Set received day to be after issuesDate

--- seeders/statusGenerator.js
+++ seeders/statusGenerator.js
@@ -12,11 +12,22 @@ const myInterface = readline.createInterface({
 let lineno = 0;
 myInterface.on('line', (line) => {
   lineno += 1;
-  console.log(`Line number ${lineno}: ${line}`);
+  // console.log(`Line number ${lineno}: ${line}`);
   const issue = line.split(',');
   const issueId = issue[0];
   const categoryId = issue[2];
-  const dateIssued = issue[4];
+  const dateIssued = issue[4].split(' ')[0];
+  // const date_resolved = new Date(dateIssued).addDays(Math.floor(Math.random() * 10));
+  // const date_received = faker.date.between(dateIssued, date_resolved).toLocaleString();
+  const date_received = new Date(dateIssued);
+  date_received.setDate(date_received.getDate() + 1);
+  console.log('Date received:', date_received);
+
+  const date_resolved = new Date(date_received);
+  const days = Math.floor(Math.random() * 14);
+  date_resolved.setDate(date_resolved.getDate() + days);
+  console.log(date_resolved);
+
 
   console.log(issueId, categoryId, dateIssued);
 });
