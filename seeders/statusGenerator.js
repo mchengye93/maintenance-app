cd4f8438ec074e19b82cab21857065640419eafb
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-19 21:37:27 -0700

    Read through issues.csv and create status

--- seeders/statusGenerator.js
+++ seeders/statusGenerator.js
@@ -1,14 +1,17 @@
 const faker = require('faker');
+const readline = require('readline');
 const fs = require('fs');
 
 const writeStream = fs.createWriteStream('status.csv');
 
 const myInterface = readline.createInterface({
-  input: fs.createReadStream('demofile1.html'),
+  input: fs.createReadStream('issues.csv'),
 });
 
 let lineno = 0;
 myInterface.on('line', (line) => {
-  lineno++;
+  lineno += 1;
   console.log(`Line number ${lineno}: ${line}`);
+  const lineArr = line.split(',');
+  console.log(lineArr);
 });
