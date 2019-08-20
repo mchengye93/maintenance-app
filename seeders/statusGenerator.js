9d5f5d3d58a97b836c33b027528e4fc2f850dd3b
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-19 21:34:47 -0700

    Read line by line of file

--- seeders/statusGenerator.js
+++ seeders/statusGenerator.js
@@ -2,3 +2,13 @@ const faker = require('faker');
 const fs = require('fs');
 
 const writeStream = fs.createWriteStream('status.csv');
+
+const myInterface = readline.createInterface({
+  input: fs.createReadStream('demofile1.html'),
+});
+
+let lineno = 0;
+myInterface.on('line', (line) => {
+  lineno++;
+  console.log(`Line number ${lineno}: ${line}`);
+});
