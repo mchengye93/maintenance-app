60acbd7ba231def3855366c7d23e1c4140852099
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-21 23:23:37 -0700

    fix dates in issueGenerator

--- seeders/issuesGenerator.js
+++ seeders/issuesGenerator.js
@@ -33,7 +33,7 @@ for (let i = 1; i <= 10; i += 1) {
       subcategoryId = faker.random.number({ min: 22, max: 23 });
       break;
   }
-  const dateIssued = faker.date.between('2019-07-25', '2019-08-21').toLocaleString();
+  const dateIssued = faker.date.between('2019-07-25', '2019-08-21').toLocaleString().split(' ')[0];
 
   const photoUrl = faker.image.imageUrl(400, 400, 'business');
 
@@ -46,7 +46,7 @@ for (let i = 1; i <= 10; i += 1) {
   let dateReceived = '';
   let contactId = '';
 
-  if (probability === 1) {
+  if (1) {
     dateReceived = new Date(dateIssued);
     const receivedDays = Math.floor(Math.random() * 3) + 1;
     dateReceived.setDate(dateReceived.getDate() + receivedDays);
@@ -94,7 +94,7 @@ for (let i = 1; i <= 10; i += 1) {
   // console.log(issueId, categoryId, dateIssued);
 
 
-  const issueRecord = `${i},${roomId},${categoryId},${subcategoryId},${photoUrl},${description},${cost},${dateIssued}, ${contactId}, ${dateReceived},${dateResolved}\n`;
+  const issueRecord = `${i},${roomId},${categoryId},${subcategoryId},${photoUrl},${description},${cost},${dateIssued},${contactId},${dateReceived},${dateResolved}\n`;
   // write some data with a base64 encoding
   //   var stockRecord = {
   //     id: i,
