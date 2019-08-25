149616945fb0e0c6fd035568cb1ad45cf77e0e44
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-24 22:21:16 -0700

    Get all pending issues by category API fix

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.get('/api/issues/vip/pending', (req, res) => {
   });
 });
 // Return all pending issues by category
-app.get('/api/issues/contact', (req, res) => {
+app.get('/api/issues/category', (req, res) => {
   issues.getAllPendingIssuesByCategory(req.body.category, (err, data) => {
     if (err) {
       res.sendStatus(500);
