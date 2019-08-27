0a123b5b59e094daa4bd517716b0025e4a07f80f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-27 00:23:04 -0700

    App.jsx file setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, Component from 'react';
+import axios from 'axios';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="app"></div>
+                <h1>Maintenance Application</h1>
+            </div>
+        )
+    }
+}
+export default App;
