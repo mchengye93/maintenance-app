70a4c329afd07932cbcb4573421765f6c68568e4
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-27 20:47:48 -0700

    API routes

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+import room from './room';
+import issue from './issue';
+import contact from './contact';
+import category from './category';
+import subcategory from './subcategory';
+
+export default {
+  issue,
+  room,
+  contact,
+  category,
+  subcategory,
+};
