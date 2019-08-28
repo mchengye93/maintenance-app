bf254b5cbfa97534fc354c03aac9054681465558
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-27 21:12:57 -0700

    Material table UI

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
+
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+        
+        };
 
     }
     componentDidMount() {
