7c7fc5c2f93876aabfd164f032882edcff5cac23
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-02 00:51:34 -0700

    SolveForm component code cleanup

--- client/src/components/SolveForm.jsx
+++ client/src/components/SolveForm.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,9 +8,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import axios from 'axios';
-
-
 
 
 class SolveForm extends Component {
@@ -19,7 +18,6 @@ class SolveForm extends Component {
         open: false,
         comment: '',
         cost: 0
-
         };
 
         this.handleClickOpen = this.handleClickOpen.bind(this);
@@ -30,21 +28,19 @@ class SolveForm extends Component {
     }
     componentDidMount() {
 
-       
 
     }
+
     handleClickOpen() {
         this.setState({open: true});
-      }
+    }
 
     handleClose() {
         this.setState({open: false});
-      }
+    }
+
     handleResolveIssue() {
-      console.log(this.state);
-      console.log(this.props);
-       
-        //Verify that all input has been defined
+
         let issue = {
             cost: this.state.cost,
             comment:this.state.comment,
@@ -58,85 +54,77 @@ class SolveForm extends Component {
         }).catch((error)=> {
             alert('Error taking issue');
         })
-            
-        
-      
+
     }
 
-  
     handleInputChange(event) {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
        
-     
         this.setState({
                 [name]: value
               });
         
-
       }
 
     render() {
-      console.log(this.state);
-      console.log(this.props);
-        
             return (
                 <div>
-                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
-                  Resolve
-                </Button>
-                <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-                  <DialogTitle id="form-dialog-title">
-                  Issue #{this.props.issue.id}
-                  </DialogTitle>
-                  <DialogContent>
-                    <DialogContentText>
-                      Room No.{this.props.issue.room_id} <br/>
-                      Category: {this.props.issue.category} <br/>
-                      Subcategory: {this.props.issue.subcategory} <br/>
-                      Issued Date: {this.props.issue.date_issued.split('T')[0]}
-                    </DialogContentText>
-                   
-                    <form> 
-                    <TextField
-                      margin="dense"
-                      id="cost"
-                      label="Cost"
-                      name="cost"
-                      onChange={this.handleInputChange}
-                      type="Number"
-                      margin="normal"
-                      variant="outlined"
-                      value = {this.state.cost}
-                    />  <br/>  
-                    <TextField
-                      id="outlined-multiline-static"
-                      label="Comments"
-                      multiline
-                      rows="4"
-                      placeholder="Repair details"
-                      margin="dense"
-                      variant="outlined"
-                      name="comment"
-                      onChange={this.handleInputChange}
-                    />
-                           
-                    </form>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button onClick={this.handleClose} color="primary">
-                      Cancel
-                    </Button>
-                    <Button onClick={this.handleResolveIssue} color="primary">
-                      Resolve
-                    </Button>
-                  </DialogActions>
-                </Dialog>
+                  <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    Resolve
+                  </Button>
+                  <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">
+                    Issue #{this.props.issue.id}
+                    </DialogTitle>
+                    <DialogContent>
+                      <DialogContentText>
+                        Room No.{this.props.issue.room_id} <br/>
+                        Category: {this.props.issue.category} <br/>
+                        Subcategory: {this.props.issue.subcategory} <br/>
+                        Issued Date: {this.props.issue.date_issued.split('T')[0]}
+                      </DialogContentText>
+                    
+                      <form> 
+                      <TextField
+                        margin="dense"
+                        id="cost"
+                        label="Cost"
+                        name="cost"
+                        onChange={this.handleInputChange}
+                        type="Number"
+                        margin="normal"
+                        variant="outlined"
+                        value = {this.state.cost}
+                      />  <br/>  
+                      <TextField
+                        id="outlined-multiline-static"
+                        label="Comments"
+                        multiline
+                        rows="4"
+                        placeholder="Repair details"
+                        margin="dense"
+                        variant="outlined"
+                        name="comment"
+                        onChange={this.handleInputChange}
+                      />
+                            
+                      </form>
+                    </DialogContent>
+                    <DialogActions>
+                      <Button onClick={this.handleClose} color="primary">
+                        Cancel
+                      </Button>
+                      <Button onClick={this.handleResolveIssue} color="primary">
+                        Resolve
+                      </Button>
+                    </DialogActions>
+                  </Dialog>
               </div>
        
             );
         }
-   
 }
+
 export default SolveForm;
