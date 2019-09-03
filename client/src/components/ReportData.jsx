3fa7ace87ebb72a85144aebc51c14239255431a0
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-02 22:14:29 -0700

    Parse all issue cost by category and month/year

--- client/src/components/ReportData.jsx
+++ client/src/components/ReportData.jsx
@@ -9,13 +9,15 @@ class ReportData extends Component {
           issues: [],
         };
         this.handleChangeStatus = this.handleChangeStatus.bind(this);
+        this.parseIssues = this.parseIssues.bind(this);
     }
     componentDidMount() {
         axios.get('/api/issues/resolved')
         .then((response)=> {
             this.setState({
                 issues: response.data
-            })
+            });
+            this.parseIssues(response.data);
         })
     }
     handleChangeStatus(e) {
@@ -24,8 +26,37 @@ class ReportData extends Component {
         
     }
     parseIssues(issues) {
-        
+        //parse through issue and calculate total cost for each category
+        let costByCategory = {};
 
+        for (let i = 0; i < issues.length; i++) {
+            let category = issues[i].category;
+            let cost = issues[i].cost;
+            let date = new Date(issues[i].date_resolved);
+           
+            let month = date.getMonth() + 1; // Since getMonth() returns month from 0-11 not 1-12
+            let year = date.getFullYear();
+            
+            let monthYear =  month + "/" + year;
+            //console.log(monthYear);
+
+            if (costByCategory[category]) {
+                //Get object and add monthyear object
+                let categoryObject = costByCategory[category];
+                
+                if (categoryObject[monthYear]) {
+                    categoryObject[monthYear] += cost;
+                } else {
+                    categoryObject[monthYear] = cost;
+                }
+                
+
+            } else {
+                costByCategory[category] = { [monthYear] : cost};
+            }
+        }
+
+        console.log(costByCategory);
     }
 
     render() {
