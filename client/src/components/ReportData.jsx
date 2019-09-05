02298d48fd4b4de79a7faef3b69104c249af0638
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-04 22:21:13 -0700

    Clean report data component

--- client/src/components/ReportData.jsx
+++ client/src/components/ReportData.jsx
@@ -12,39 +12,16 @@ class ReportData extends Component {
         super(props);
         this.state = {
           issues: [],
-          
-        //   data: 
-        //    [ {
-        //        label: '7/19',
-        //         values: [{x: 'Electrical', y: 10}, {x: 'Plumbing', y: 4}, {x: 'Fixture', y: 3}]
-        //     },
-        //     {
-        //         label: '8/19',
-        //         values: [{x: 'Electrical', y: 100}, {x: 'Plumbing', y: 4}, {x: 'Fixture', y: 3}]
-        //         },
-        //     ],
-     
+
         };
         this.handleChangeStatus = this.handleChangeStatus.bind(this);
-        this.parseIssues = this.parseIssues.bind(this);
         this.parseCosts = this.parseCosts.bind(this);
     }
     componentDidMount() {
-        // axios.get('/api/issues/resolved')
-        // .then((response)=> {
-        //     this.setState({
-        //         issues: response.data
-        //     });
-        //     this.parseIssues(response.data);
-        // })
-
+     
         axios.get('/api/cost/category')
         .then((response)=> {
-            // this.setState({
-            //     cost: response.data
-            // });
             this.parseCosts(response.data);
-            //this.parseIssues(response.data);
         })
     }
     handleChangeStatus(e) {
@@ -55,11 +32,6 @@ class ReportData extends Component {
     parseCosts(costs) {
         let costByCategoryMonth = {};
 
-        // {
-        //     "month_resolved": "2019-07-01T07:00:00.000Z",
-        //     "category": "Fixture",
-        //     "sum": "916"
-        // },
 
         for (let i = 0; i < costs.length; i++) {
             let category = costs[i]["category"];
@@ -82,55 +54,17 @@ class ReportData extends Component {
         this.setState({cost: monthResults});
 
     }
-    parseIssues(issues) {
-        //parse through issue and calculate total cost for each category
-        let costByCategory = {};
-
-        for (let i = 0; i < issues.length; i++) {
-            let category = issues[i].category;
-            let cost = issues[i].cost;
-            let date = new Date(issues[i].date_resolved);
-           
-            let month = date.getMonth() + 1; // Since getMonth() returns month from 0-11 not 1-12
-            let year = date.getFullYear();
-            
-            let monthYear =  month + "/" + year;
-            //console.log(monthYear);
-
-            if (costByCategory[category]) {
-                //Get object and add monthyear object
-                let categoryObject = costByCategory[category];
-                
-                if (categoryObject[monthYear]) {
-                    categoryObject[monthYear] += cost;
-                } else {
-                    categoryObject[monthYear] = cost;
-                }
-                
-
-            } else {
-                costByCategory[category] = { [monthYear] : cost};
-            }
-        }
-
-        console.log(costByCategory);
-    }
 
     render() {
-        console.log(this.state);
+ 
         if (this.state.cost !== undefined){
-            console.log("Cost is not empty!");
+          
             return(
             
                 <div id="report">
-     
-                    {/* {pieCharts} */}
-                    {/* <BarChart/> */}
-                    <GroupedBarChart data={this.state.cost}/>
-                     
+                    <GroupedBarChart data={this.state.cost}/>       
                 </div>
-         
-            
+           
             );
         } else {
             return(
