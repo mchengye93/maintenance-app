2532320783593e683bd608fa4baed7c135fe2091
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-08 22:00:27 -0700

    contactrow data component implement

--- client/src/components/ContactRowData.jsx
+++ client/src/components/ContactRowData.jsx
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 
-import TakeIssueForm from './TakeIssueForm.jsx';
-import SolveForm from './SolveForm.jsx';
 
-class IssueRowData extends Component {
+class ContactRowData extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -18,49 +16,17 @@ class IssueRowData extends Component {
 
     render() {
        
-        if(this.props.status == 0) {
+   
             return (
-                <TableRow key={this.props.issue.id}>
-                    <TableCell>{this.props.issue.room_id}</TableCell>
-                    <TableCell>{this.props.issue.category}</TableCell>
-                    <TableCell>{this.props.issue.subcategory}</TableCell>
-                    <TableCell>{this.props.issue.date_issued.split('T')[0]}</TableCell>
-                    <TableCell>
-                        <TakeIssueForm issue= {this.props.issue}  changeIssueStatus = {this.props.changeIssueStatus}/>
-                    </TableCell>
+                <TableRow key={this.props.contact.id}>
+                    <TableCell>{this.props.contact.name}</TableCell>
+                    <TableCell>{this.props.contact.category}</TableCell>
+                    <TableCell>{this.props.contact.phone}</TableCell>
+                    <TableCell>{this.props.contact.email}</TableCell>
                 </TableRow>
                              
             );
-        } else if (this.props.status == 1) {
-            return (
-                <TableRow key={this.props.issue.id}>
-                    <TableCell>{this.props.issue.room_id}</TableCell>
-                    <TableCell>{this.props.issue.category}</TableCell>
-                    <TableCell>{this.props.issue.subcategory}</TableCell>
-                    <TableCell>{this.props.issue.date_issued.split('T')[0]}</TableCell>
-                    <TableCell>{this.props.issue.date_received.split('T')[0]}</TableCell>
-                    <TableCell>{this.props.issue.name}</TableCell>
-                    <TableCell>
-                        <SolveForm issue= {this.props.issue} changeIssueStatus = {this.props.changeIssueStatus}/>
-                    </TableCell>
-                </TableRow>
-                             
-            );
-        } else if (this.props.status == 2) {
-            return (
-                <TableRow key={this.props.issue.id}>
-                    <TableCell>{this.props.issue.room_id}</TableCell>
-                    <TableCell>{this.props.issue.category}</TableCell>
-                    <TableCell>{this.props.issue.subcategory}</TableCell>
-                    <TableCell>{this.props.issue.date_issued.split('T')[0]}</TableCell>
-                    <TableCell>{this.props.issue.date_received.split('T')[0]}</TableCell>
-                    <TableCell>{this.props.issue.date_resolved.split('T')[0]}</TableCell>
-                    <TableCell>{this.props.issue.name}</TableCell>
-                    <TableCell>{this.props.issue.cost}</TableCell>
-                </TableRow>                 
-            );
-        }
     }
 }
 
-export default IssueRowData;
+export default ContactRowData;
