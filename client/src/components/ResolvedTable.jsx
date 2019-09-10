117cbd6f080bd5eed7819cf6484177149dbc4367
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-09 22:08:22 -0700

    resolvetable rename subcategory

--- client/src/components/ResolvedTable.jsx
+++ client/src/components/ResolvedTable.jsx
@@ -40,7 +40,7 @@ class ResolvedTable extends Component {
                     <TableRow>
                         <TableCell align='center'>Room ID</TableCell>
                         <TableCell align='center'>Category</TableCell>
-                        <TableCell align='center'>SubCategory</TableCell>
+                        <TableCell align='center'>Subcategory</TableCell>
                         <TableCell align='center'>Issued Date</TableCell>
                         <TableCell align='center'>Received Date</TableCell>
                         <TableCell align='center'>Resolved Date</TableCell> 
