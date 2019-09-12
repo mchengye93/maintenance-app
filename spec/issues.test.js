8ac2fa28add95a3e2ea8b28fabe3c8ae8f4803be
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-11 22:05:08 -0700

    Test dateReceived vs dateIssued for inprogress issues

--- spec/issues.test.js
+++ spec/issues.test.js
@@ -110,9 +110,7 @@ describe('Test Suite: Get Pending Issue', () => {
 
         done();
       }));
-});
 
-describe('Test Suite: Get In Progress Issue', () => {
   test('It properly fetches all pending issues by categoryId=1',
     async done => request(app)
       .get('/api/issues/pending/category?categoryId=1')
@@ -142,6 +140,70 @@ describe('Test Suite: Get In Progress Issue', () => {
 
         expect(dateIssued).toBeDefined();
         expect(typeof dateIssued).toBe('string');
+
+
+        done();
+      }));
+});
+
+
+describe('Test Suite: Get In Progress Issue', () => {
+  test('It properly fetches all inProgress issues by categoryId=1',
+    async done => request(app)
+      .get('/api/issues/received/category?categoryId=1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(({ body }) => {
+        const firstIssue = body[0];
+        const { id } = firstIssue;
+        const roomId = firstIssue.room_id;
+        const categoryId = firstIssue.category_id;
+        const subcategoryId = firstIssue.subcategory_id;
+        const dateIssued = firstIssue.date_issued;
+        const dateReceived = firstIssue.date_received;
+
+        expect(id).toBeDefined();
+        expect(typeof id).toBe('number');
+
+        expect(roomId).toBeDefined();
+        expect(typeof roomId).toBe('number');
+
+        expect(categoryId).toBeDefined();
+        expect(typeof categoryId).toBe('number');
+        expect(categoryId).toBe(1);
+
+        expect(subcategoryId).toBeDefined();
+        expect(typeof subcategoryId).toBe('number');
+
+        expect(dateIssued).toBeDefined();
+        expect(typeof dateIssued).toBe('string');
+
+        expect(dateReceived).toBeDefined();
+        expect(typeof dateReceived).toBe('string');
+
+        done();
+      }));
+
+  test('For inProgress issue dateReceived >= dateIssue',
+    async done => request(app)
+      .get('/api/issues/received/category?categoryId=1')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(({ body }) => {
+        const firstIssue = body[0];
+
+        const dateIssued = firstIssue.date_issued;
+        const dateReceived = firstIssue.date_received;
+
+        const dateDifference = new Date(dateReceived) - new Date(dateIssued);
+
+        expect(dateIssued).toBeDefined();
+        expect(typeof dateIssued).toBe('string');
+
+        expect(dateReceived).toBeDefined();
+        expect(typeof dateReceived).toBe('string');
+
+        expect(dateDifference).toBeGreaterThanOrEqual(0);
 
         done();
       }));
