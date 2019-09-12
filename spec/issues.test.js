b7ef2a15e2dd5f24d8f6642896eb7bdd8a777c7c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-11 21:50:46 -0700

    Get pending issues

--- spec/issues.test.js
+++ spec/issues.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../server/index');
 
-describe('Test Suite: CRUD Issue', () => {
+describe('Test Suite: Get Issue', () => {
   test('It properly fetches all issues',
     async done => request(app)
       .get('/api/issues')
@@ -73,7 +73,42 @@ describe('Test Suite: CRUD Issue', () => {
       .expect('Content-Type', /json/)
       .expect(400)
       .then(({ body }) => {
-        console.log(body);
+        done();
+      }));
+});
+
+
+describe('Test Suite: Get Pending Issue', () => {
+  test('It properly fetches all issues',
+    async done => request(app)
+      .get('/api/issues')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(({ body }) => {
+        const firstIssue = body[0];
+        const { id } = firstIssue;
+        const roomId = firstIssue.room_id;
+        const categoryId = firstIssue.category_id;
+        const subcategoryId = firstIssue.subcategory_id;
+        const dateIssued = firstIssue.date_issued;
+
+
+        expect(id).toBeDefined();
+        expect(typeof id).toBe('number');
+
+        expect(roomId).toBeDefined();
+        expect(typeof roomId).toBe('number');
+
+        expect(categoryId).toBeDefined();
+        expect(typeof categoryId).toBe('number');
+
+        expect(subcategoryId).toBeDefined();
+        expect(typeof subcategoryId).toBe('number');
+
+        expect(dateIssued).toBeDefined();
+        expect(typeof dateIssued).toBe('string');
+
+
         done();
       }));
 });
