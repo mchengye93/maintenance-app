6abd81076a9b7247d1feef7daf62f31937e4755d
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-11 21:40:22 -0700

    test getAll issues implemented

--- spec/issues.test.js
+++ spec/issues.test.js
@@ -2,16 +2,38 @@ const request = require('supertest');
 const app = require('../server/index');
 
 describe('Test Suite: CRUD Issue', () => {
-  it('It properly fetches all issues',
-    () => request(app)
+  test('It properly fetches all issues',
+    async done => request(app)
       .get('/api/issues')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(({ body }) => {
-        console.log(body);
-        const { id } = body;
+        const firstIssue = body[0];
+        const { id } = firstIssue;
+        const roomId = firstIssue.room_id;
+        const categoryId = firstIssue.category_id;
+        const subcategoryId = firstIssue.subcategory_id;
+        const dateIssued = firstIssue.date_issued;
+
 
         expect(id).toBeDefined();
         expect(typeof id).toBe('number');
+
+        expect(roomId).toBeDefined();
+        expect(typeof roomId).toBe('number');
+
+        expect(categoryId).toBeDefined();
+        expect(typeof categoryId).toBe('number');
+
+        expect(subcategoryId).toBeDefined();
+        expect(typeof subcategoryId).toBe('number');
+
+        expect(dateIssued).toBeDefined();
+        expect(typeof dateIssued).toBe('string');
+
+
+        expect();
+
+        done();
       }));
 });
