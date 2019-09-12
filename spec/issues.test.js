f6e7a118b351ae78b9d8e1f612ef105d962b1c13
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-11 21:09:07 -0700

    Test get all issues

--- spec/issues.test.js
+++ spec/issues.test.js
@@ -1,2 +1,16 @@
 const request = require('supertest');
 const app = require('../server/index');
+
+describe('Test Suite: CRUD Issue', () => {
+  it('It properly fetches all issues',
+    () => request(app)
+      .get('/api/issues')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(({ body }) => {
+        const { id } = body;
+
+        expect(id).toBeDefined();
+        expect(typeof id).toBe('number');
+      }));
+});
