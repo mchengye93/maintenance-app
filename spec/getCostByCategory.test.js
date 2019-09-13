e3f3008cb3d161f787b53f5a3eb09596935b0c91
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-12 22:44:18 -0700

    API should return sum of total cost by category of last two months

--- spec/getCostByCategory.test.js
+++ spec/getCostByCategory.test.js
@@ -3,12 +3,13 @@ const request = require('supertest');
 const app = require('../server/index');
 
 describe('Test Suite: Get Cost By Category', () => {
-  test('It properly fetches all contacts',
+  test('It properly fetches all cost by category of two months',
     async done => request(app)
       .get('/api/cost/category')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(({ body }) => {
+        expect(body.length).toBe(10);
         done();
       }));
 });
